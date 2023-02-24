7d9cc713e4d80b62989f596bd50093ed013bfed2
池ノ谷優太 <you1207.ike@gmail.com>
2023-02-24 12:03:03 +0900

    deploy test2

--- src/ViewComponents/AppHeader.js
+++ src/ViewComponents/AppHeader.js
@@ -1,7 +1,7 @@
 /*アプリ自体のヘッダー*/
 import React from "react";
 import VideoEngine from "../Modules/VideoEngine.ts";
-//import testImage from "../Aseets/logo192.png";
+import testImage from "../Aseets/logo192.png";
 
 function AppHeader() {
 
@@ -34,6 +34,7 @@ function AppHeader() {
 		<div id="appHeader"
 			style={appHeaderStyle}
 		>
+			<img src={testImage}/>
 			<button
 				onClick={testClick}
 			>
